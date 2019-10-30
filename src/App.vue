ed3539b814ccb46342e2976a461d505892424938
suzuki_ta <st09d18@icloud.com>
2019-10-31 00:52:26 +0900

    jsonファイル取得確認

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,42 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
-
     <v-content>
-      <HelloWorld/>
+      {{cardlist}}
+      {{deck}}
+      <SearchArea/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import SearchArea from './components/SearchArea';
+import Store from 'electron-store';
+import path from 'path';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    SearchArea,
+  },
+  created: function() {
+    this.cardstore = new Store({
+      cwd:  path.resolve(), 
+      name: "cardlist"
+    })
+    this.configstore = new Store({
+      cwd:  path.resolve(), 
+      name: "config"
+    })
+  },
+  mounted: function() {
+    this.cardlist = this.configstore.get("Card", {"mu":"ri"})
+    this.deck = this.configstore.get("Card.Deck", {"mu":"ri"})
   },
   data: () => ({
-    //
+    deck: null,
+    cardlist: {},
+    cardstore: null,
+    configstore: null,
   }),
 };
 </script>
