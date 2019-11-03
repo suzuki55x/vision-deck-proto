17866f94aad184136f437d81b66773e1a1dc665d
suzuki_ta <st09d18@icloud.com>
2019-11-04 04:51:15 +0900

    update 検索部分、とりあえず作成

--- src/components/SearchArea.vue
+++ src/components/SearchArea.vue
@@ -13,24 +13,24 @@
         <v-form>
           <v-layout justify-center>
             <v-card class="subheading mx-3">
-              <v-text-field label="No" name="cardNo" outlined dense clearable></v-text-field>
-              <v-text-field label="文字列" name="cardTxt" outlined dense clearable></v-text-field>
-              <v-select label="キャラクター" chips multiple outlined dense clearable></v-select>
-              <v-select label="カードセット" chips multiple outlined dense clearable></v-select>
-              <v-select label="種類" chips multiple outlined dense clearable></v-select>
+              <v-text-field label="No" name="cardNo" outlined dense clearable hide-details></v-text-field>
+              <v-text-field label="文字列" name="cardTxt" outlined dense clearable hide-details></v-text-field>
+              <v-select label="キャラクター" chips multiple outlined dense clearable hide-details></v-select>
+              <v-select label="カードセット" chips multiple outlined dense clearable hide-details></v-select>
+              <v-select label="種類" chips multiple outlined dense clearable hide-details></v-select>
             </v-card>
             <v-card class="subheading mx-3">
-              <v-select label="種類" chips multiple outlined dense clearable></v-select>
-              <v-select label="種類" chips multiple outlined dense clearable></v-select>
-              <v-select label="種類" chips multiple outlined dense clearable></v-select>
-              <v-select label="種類" chips multiple outlined dense clearable></v-select>
+              <v-select label="ノード" chips multiple outlined dense clearable hide-details></v-select>
+              <v-select label="コスト" chips multiple outlined dense clearable hide-details></v-select>
+              <v-select label="攻撃" chips multiple outlined dense clearable hide-details></v-select>
+              <v-select label="耐久" chips multiple outlined dense clearable hide-details></v-select>
             </v-card>
             <v-card class="subheading mx-3">
-              <v-select label="種類" chips multiple outlined dense clearable></v-select>
-              <v-select label="種類" chips multiple outlined dense clearable></v-select>
-              <v-select label="種類" chips multiple outlined dense clearable></v-select>
-              <v-select label="種類" chips multiple outlined dense clearable></v-select>
-              <v-select label="種類" chips multiple outlined dense clearable></v-select>
+              <v-select label="特殊効果" chips multiple outlined dense clearable hide-details></v-select>
+              <v-select label="戦術" chips multiple outlined dense clearable hide-details></v-select>
+              <v-select label="種族" chips multiple outlined dense clearable hide-details></v-select>
+              <v-select label="範囲" chips multiple outlined dense clearable hide-details></v-select>
+              <v-select label="期間" chips multiple outlined dense clearable hide-details></v-select>
             </v-card>
           </v-layout>
         </v-form>
