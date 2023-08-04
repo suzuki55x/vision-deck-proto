f6ecbd0affbcdfbe95c1fae21df8b32f515a7e86
suzuki_ta@od <st09d18@icloud.com>
2023-08-04 17:58:32 +0900

    Add カードリストにN/C表示

--- src/components/CardListVirtualScroll.vue
+++ src/components/CardListVirtualScroll.vue
@@ -3,7 +3,7 @@
     <template v-slot:default="{ item }">
       <v-list-item dense :class="getBgColor(item)">
         <v-row>
-          <v-col cols="2" class="text-caption" v-if="is_decklist && 'SheetNum' in item">
+          <v-col cols="1" class="text-caption" v-if="is_decklist && 'SheetNum' in item">
             <v-avatar size="20" color="#2196F3" class="white--text">
               {{ item.SheetNum }}
             </v-avatar>
@@ -12,10 +12,17 @@
           <v-col cols="2" class="text-caption">
             No.{{ item.No }}
           </v-col>
-          <v-col cols="6">
+
+          <v-col cols="6" class="pl-0">
             <div class="text-truncate">
               {{ item.Name }}
             </div>
+          </v-col>
+
+          <v-col :cols="is_decklist ? 1 : 2" class="text-caption px-0">
+            <v-layout>
+              <v-flex class="">N{{ item.Node || '' }}/C{{ item.Cost || '' }}</v-flex>
+            </v-layout>
           </v-col>
 
           <v-col cols="2" class="text-caption">
