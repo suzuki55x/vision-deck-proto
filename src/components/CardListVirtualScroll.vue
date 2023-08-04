5089a784f2a76481b2cd2d2917bf98d9dc1f17ef
suzuki_ta@od <st09d18@icloud.com>
2023-08-04 17:24:38 +0900

    Add カードリストの背景を色分け

--- src/components/CardListVirtualScroll.vue
+++ src/components/CardListVirtualScroll.vue
@@ -1,7 +1,7 @@
 <template>
   <v-virtual-scroll bench="1" :items="cardlist" height="520" item-height="42">
     <template v-slot:default="{ item }">
-      <v-list-item dense>
+      <v-list-item dense :class="getBgColor(item)">
         <v-row>
           <v-col cols="2" class="text-caption" v-if="is_decklist && 'SheetNum' in item">
             <v-avatar size="20" color="#2196F3" class="white--text">
@@ -14,7 +14,7 @@
           </v-col>
           <v-col cols="6">
             <div class="text-truncate">
-            {{ item.Name }}
+              {{ item.Name }}
             </div>
           </v-col>
 
@@ -30,6 +30,19 @@
 
 <script>
 export default {
+  methods: {
+    getBgColor(item) {
+      if (item.Type == 'Character') {
+        return 'yellow lighten-5';
+      } else if (item.Type == 'Spell') {
+        return 'pink lighten-5';
+      } else if (item.Type == 'Command') {
+        return 'blue-grey lighten-4';
+      } else {
+        return '';
+      }
+    },
+  },
   props: {
     cardlist: Array,
     is_decklist: Boolean,
